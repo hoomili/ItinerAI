0823954d26a4a36d2e65d56e8d03a0b3ca10b428
hoomili <hasadian@hotmail.com>
2023-06-12 12:17:45 -0700

    create component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.scss';
+import ItineraryListItem from './components/itinerarylistitem';
 
 function App() {
   return (
     <div className="App">
-      <h1>Project init</h1>
+      <ItineraryListItem/>
     </div>
   );
 }
