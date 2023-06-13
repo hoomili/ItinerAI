0a49c265011c0e926111fbc00f9067850d4abebd
hoomili <hasadian@hotmail.com>
2023-06-13 14:59:49 -0700

    maps points based on data recieved from chatGPT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.scss';
 import Homepage from './components/Homepage';
 import ItineraryListItem from './components/itinerarylistitem';
+import { pointsContext } from "./components/context";
 
 function App() {
+  const [aiData, setAiData] = useState([]);
+
+
+
   return (
     <div className="App">
       <h1>Project init</h1>
-      <Homepage />
-      <ItineraryListItem/>
+      <Homepage setAiData={setAiData}/>
+      {aiData.length > 0 ? <ItineraryListItem aiData={aiData}/>: ''}
+
     </div>
   );
 }
--- frontend/src/components/itinerarylistitem.jsx
+++ frontend/src/components/itinerarylistitem.jsx
@@ -8,48 +8,47 @@ import {
 import axios from "axios";
 
 import "../styles/itinerarylistitem.scss";
-const ItineraryListItem = () => {
+
+
+const ItineraryListItem = (props) => {
   const [map, setMap] = useState(null);
-  const [info, setInfo] = useState([]);
-  
+  const itinerary = props.aiData[0].itineraryText
+  const points = props.aiData[0].keyLocations
+  const accomodation = props.aiData[0].accomodation
+  const city = props.aiData[0].city
+  const country = props.aiData[0].country
 
-  const config = {
-    method: "get",
-    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Blue%20Water%20Cafe%20vancouver%20canada&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${process.env.REACT_APP_NEXT_PUBLIC_MAP_API_KEY}`,
-    headers: {},
-  };
-  useEffect(() => {
-    axios(config)
-      .then(function (response) {
-        setInfo([response.data]);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, [])
 
-  const center = {
-    lat: 49.2756,
-    lng: -123.1208,
-  };
 
-  const positions = [
-    { lat: 49.2761, lng: -123.1228, name: "Blue Water Cafe" },
-    { lat: 49.2764, lng: -123.1219, name: "Blue Water Cafe" },
-    { lat: 49.277, lng: -123.1227, name: "Blue Water Cafe" },
-    { lat: 49.2748, lng: -123.1216, name: "Blue Water Cafe" },
-    { lat: 49.2753, lng: -123.1213, name: "Blue Water Cafe" },
-  ];
+  // const config = {
+  //   method: "get",
+  //   url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Blue%20Water%20Cafe%20vancouver%20canada&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${process.env.REACT_APP_NEXT_PUBLIC_MAP_API_KEY}`,
+  //   headers: {},
+  //   crossdomain: true
+  // };
+  // useEffect(() => {
+    // axios(config)
+    //   .then(function (response) {
+    //     setInfo([response.data]);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    
+  // }, [])
 
+
+  // get the api key for the map
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_NEXT_PUBLIC_MAP_API_KEY,
   });
 
+  // set boundry for the map
   const onLoad = useCallback((map) => {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    for (let index = 0; index < positions.length; index++) {
-      const element = positions[index];
+    const bounds = new window.google.maps.LatLngBounds(accomodation);
+    for (let i = 0; i < points.length; i++) {
+      const element = points[i];
+      console.log('each point', element);
       const newCoord = new window.google.maps.LatLng(element.lat, element.lng);
       bounds.extend(newCoord);
     }
@@ -61,50 +60,22 @@ const ItineraryListItem = () => {
     setMap(null);
   }, []);
 
-  const newPositions = positions.map((position) => {
-    return <MarkerF position={position} />;
+  // setting up the points for the map
+  const newPoints = points.map((position) => {
+    return <MarkerF position={position} label={points.title}/>;
   });
+  
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:8080/")
-  //   .then((response) => {
-  //     console.log(response)
-  //   })
-  // })
-  // useEffect(() => {
-  // })
   if (!isLoaded) {
     return <div>loading...</div>;
-  }
+  } 
 
   return (
     <div className="itineray-list--item">
       <div className="itineray-list--item-description">
-        <h1>Trip to Vancouver</h1>
+        <h1>Awesome Trip to {city}, {country} </h1>
         <article>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          dolorem minus, accusantium itaque nobis odit recusandae voluptas.
-          Provident illo quaerat ea est reprehenderit iure, natus, porro
-          excepturi ratione vel, totam incidunt! Reprehenderit veritatis libero
-          consectetur earum? Esse nesciunt ab, modi itaque consectetur sed ipsam
-          aut non fuga nam! Adipisci vel eum aliquid voluptatum in ut saepe sint
-          optio! A, non temporibus. Eveniet, perspiciatis quisquam commodi nobis
-          aspernatur ratione reiciendis exercitationem! Saepe vitae odio soluta
-          minima aperiam in repellat perferendis cupiditate commodi architecto
-          cum, laudantium dolorum ullam unde, cumque blanditiis animi pariatur
-          mollitia optio ab culpa suscipit delectus dicta. Consectetur quo
-          soluta, harum assumenda et temporibus excepturi quidem dolores
-          similique laboriosam quasi maxime eligendi dignissimos fuga dolorum
-          quia neque explicabo laborum omnis. Officiis quam non numquam,
-          veritatis in ea. Modi, sed. Sapiente quisquam sit, rerum deserunt
-          exercitationem ducimus ratione nisi ipsam, enim mollitia eligendi eius
-          magnam iure itaque tenetur iusto quasi officiis cumque, quod vitae
-          officia illo rem ad. Architecto nostrum sunt quas itaque! Id animi
-          pariatur laborum, quam distinctio facilis autem cupiditate perferendis
-          dicta rerum, soluta nulla praesentium voluptatum magnam fugit
-          repudiandae quod eos sint illum fuga doloribus iusto quas placeat!
-          Quasi natus numquam nobis quaerat laborum quisquam eos aliquid.
-          {/* {info} */}
+          {itinerary}
         </article>
       </div>
       <GoogleMap
@@ -113,7 +84,11 @@ const ItineraryListItem = () => {
         onUnmount={onUnmount}
         mapContainerClassName="itineray-list--item-map"
       >
-        {newPositions}
+        <MarkerF position={accomodation} icon={{
+          url: (require('./../styles/hotel.png')),
+          scaledSize: new window.google.maps.Size(30, 30),
+            }}/>
+        {newPoints}
       </GoogleMap>
     </div>
   );
