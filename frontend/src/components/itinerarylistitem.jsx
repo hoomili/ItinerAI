48d4424bb8848f1e6c89eeec86a1646b9d755fbb
hoomili <hasadian@hotmail.com>
2023-06-13 09:29:43 -0700

    multiple point on the map with auto zoom

--- frontend/src/components/itinerarylistitem.jsx
+++ frontend/src/components/itinerarylistitem.jsx
@@ -1,24 +1,86 @@
-import React, { useEffect, useState } from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-import axios from 'axios'
+import { useEffect, useState, useCallback } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  InfoWindowF,
+} from "@react-google-maps/api";
+import axios from "axios";
 
 import "../styles/itinerarylistitem.scss";
 const ItineraryListItem = () => {
-  const [points, setPoints] = useState([])
+  const [map, setMap] = useState(null);
+  const [info, setInfo] = useState([]);
+  
 
+  const config = {
+    method: "get",
+    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Blue%20Water%20Cafe%20vancouver%20canada&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${process.env.REACT_APP_NEXT_PUBLIC_MAP_API_KEY}`,
+    headers: {},
+  };
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        setInfo([response.data]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [])
+
+  const center = {
+    lat: 49.2756,
+    lng: -123.1208,
+  };
+
+  const positions = [
+    { lat: 49.2761, lng: -123.1228, name: "Blue Water Cafe" },
+    { lat: 49.2764, lng: -123.1219, name: "Blue Water Cafe" },
+    { lat: 49.277, lng: -123.1227, name: "Blue Water Cafe" },
+    { lat: 49.2748, lng: -123.1216, name: "Blue Water Cafe" },
+    { lat: 49.2753, lng: -123.1213, name: "Blue Water Cafe" },
+  ];
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_NEXT_PUBLIC_MAP_API_KEY,
   });
 
+  const onLoad = useCallback((map) => {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    for (let index = 0; index < positions.length; index++) {
+      const element = positions[index];
+      const newCoord = new window.google.maps.LatLng(element.lat, element.lng);
+      bounds.extend(newCoord);
+    }
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
 
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  const newPositions = positions.map((position) => {
+    return <MarkerF position={position} />;
+  });
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:8080/")
+  //   .then((response) => {
+  //     console.log(response)
+  //   })
+  // })
+  // useEffect(() => {
+  // })
   if (!isLoaded) {
     return <div>loading...</div>;
   }
+
   return (
     <div className="itineray-list--item">
       <div className="itineray-list--item-description">
-        <h1>Lorem, ipsum dolor.</h1>
+        <h1>Trip to Vancouver</h1>
         <article>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
           dolorem minus, accusantium itaque nobis odit recusandae voluptas.
@@ -42,16 +104,17 @@ const ItineraryListItem = () => {
           dicta rerum, soluta nulla praesentium voluptatum magnam fugit
           repudiandae quod eos sint illum fuga doloribus iusto quas placeat!
           Quasi natus numquam nobis quaerat laborum quisquam eos aliquid.
+          {/* {info} */}
         </article>
       </div>
       <GoogleMap
-        zoom={10}
-        center={{ lat: 44, lng: -80 }}
+        zoom={18}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
         mapContainerClassName="itineray-list--item-map"
       >
-        <MarkerF position={{ lat: 44, lng: -80 }}/>
+        {newPositions}
       </GoogleMap>
-
     </div>
   );
 };
