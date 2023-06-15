8633b576a33abf4aa4041d4481eda8c8fabdb828
hoomili <hasadian@hotmail.com>
2023-06-15 14:20:27 -0700

    prompt change for openAi API

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -18,14 +18,20 @@ router.post("/completions", async (req, res) => {
   const prompt = `Create a fantastic travel itinerary based on the following information. Your client will be taking a trip to ${city}, ${country} for ${numDays} days with a daily budget of ${dailyBudget} dollars. The traveler is interested in ${interests}. Make sure your response takes into consideration the interests of your client, but also suggests other popular attractions and landmarks. Your itinerary should also include suggested accommodations for each different city you suggest.
   
     Return the information in JSON format as an object with the following key value pairs and format.
-  
-    'itinerary_text': HTML formatted text that is well-designed and spaced appropriately. It should include the entirety of the written itinerary you have developed. Have each of your suggestions to be bold in the HTML.
-  
+    Put each day in it's own object similar to the example. include the accommodation as an object which contains a suggested accommodation option in the form of a key-value pair. This should be noted as it's own object in your response.
     EXAMPLE:
-    "itinerary_text":
-    "Day 1: details
-    Day 2: details
-    Day 3: details"
+    "itinerary": [
+      {Day1}, {Day2}
+    ],
+    "accommodation": {
+      "title": "Hotel Vancouver"
+    }
+
+
+    Each day should include the following:
+    'itinerary_text': HTML formatted text that is well-designed and spaced appropriately. It should include the entirety of the written itinerary you have developed. make the title to have <h3> tag, and each suggestion explaination in an ordered list. Have each of your key locations and restauraunts to be bold in the HTML.
+    
+  
   
     'key_locations': an object which contains the title of key locations suggested in the itinerary. Select at least two key locations per day in the itinerary and share them in your response with me in the below example format.
   
@@ -33,25 +39,34 @@ router.post("/completions", async (req, res) => {
     "key_locations": [{
       "title": "Capilano Suspension Bridge Park" 
   }, 
-  'restauraunts': an array which contains the suggested restauraunts in the itinerary per day. Select at least 3 restauraunts per day in the itinerary and share them in your response with me in the below example format.
+  'restaurants': an array which contains the suggested restaurants in the itinerary per day. Select at least 2 restaurants per day in the itinerary and share them in your response with me in the below example format.
   
   EXAMPLE:
-  "restauraunts": [{
-    "breakfast": "some restauraunts" 
-    "lunch": "some restauraunts" 
-    "dinner": "some restauraunts"
+  "restaurants": [{
+    "title": "some restaurants" 
   }, 
   {
-    "breakfast": "some restauraunts" 
-    "lunch": "some restauraunts" 
-    "dinner": "some restauraunts"
+    "title": "some restaurants" 
   }]
   
-    'accommodation': an object which contains a suggested accommodation option in the form of a key-value pair. This should be noted as it's own object in your response.
-  
-    EXAMPLE:
-    "accommodation": {
-      "title": "Hotel Vancouver"
+
+  Final result should look like this
+    {
+      "itinerary": [
+        {
+          'itinerary_text',
+          "key_locations"
+          "restaurants"
+        }, 
+        {
+          'itinerary_text',
+          "key_locations"
+          "restaurants"
+        }
+      ],
+      "accommodation": {
+        "title": "Hotel Vancouver"
+      }
     }
   
     DO NOT repeat suggested activities or locations and please make sure the itinerary text is not repetitive in nature.
@@ -105,20 +120,19 @@ router.post("/completions", async (req, res) => {
     });
 
     const jsonData = JSON.parse(response.data.choices[0].message.content);
-    const itineraryText = jsonData.itinerary_text;
-    const keyLocations = jsonData.key_locations;
+    const itinerary = jsonData.itinerary;
     const accommodation = jsonData.accommodation;
-    const restaurants = jsonData.restaurants;
 
-    console.log("jsonData:", jsonData);
+
+    // console.log("jsonData:", jsonData);
+    // console.log("itinerary text:", itinerary);
     // console.log("itinerary text:", itineraryText);
     // console.log("key locations:", keyLocations);
     // console.log("accomodation suggestion:", accommodation);
-    console.log("restaurants suggestion:", restaurants);
-    const locations = [];
-    const breakfast = [];
-    const lunch = [];
-    const dinner = [];
+    // console.log("restaurants suggestion:", itinerary.restaurants);
+    const locationsPerDay = [];
+    const itineraryList = [];
+
 
     const config = (title) => {
       return ({method: "get",
@@ -128,36 +142,31 @@ router.post("/completions", async (req, res) => {
     const stayData = await axios(config(accommodation.title));
     const stay = stayData.data.candidates[0];
 
-
-    // get location info for all the keyLocations
-    for (let i = 0; i < keyLocations.length; i++) {
-      const element = keyLocations[i].title;
-      const placesData = await axios(config(element));
-      locations.push(placesData.data.candidates[0]);
+    for (let i = 0; i < itinerary.length; i++) {
+      const element = itinerary[i];
+      const dayLocations = []
+      itineraryList.push(element.itinerary_text)
+      for (const iterator of element.key_locations) {
+        const locationData = await axios(config(iterator.title))
+        dayLocations.push(locationData.data.candidates[0])
+      }
+      for (const iterator of element.restaurants) {
+        const locationData = await axios(config(iterator.title))
+        dayLocations.push(locationData.data.candidates[0])
+      }
+      locationsPerDay.push(dayLocations)
     }
+    // console.log('locations for days', locationsPerDay)
 
-  
-    for (const restaurant of restaurants) {
-      const breakfastData = await axios(config(restaurant.breakfast))
-      const lunchData = await axios(config(restaurant.lunch))
-      const dinnerData = await axios(config(restaurant.dinner))
-      breakfast.push(breakfastData.data.candidates[0])
-      lunch.push(lunchData.data.candidates[0])
-      dinner.push(dinnerData.data.candidates[0])
-    }
     
 
     const responseData = {
-      itineraryText,
-      keyLocations,
       accommodation,
       city,
       country,
-      locations,
+      locationsPerDay,
       stay,
-      breakfast,
-      lunch,
-      dinner,
+      itineraryList,
     };
 
     res.json(responseData);
