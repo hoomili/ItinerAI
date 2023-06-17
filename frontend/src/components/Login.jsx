74c1dc2282c837e694bc71e04acc9c4e27969592
Kamila Lemaire <kb.lemaire@gmail.com>
2023-06-16 20:19:33 -0600

    Update styling for Login and Register to match Create Itinerary

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,56 +1,72 @@
-import { useContext, useState, useEffect } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
-import { useNavigate } from 'react-router-dom';
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
 import "../styles/Login.scss";
 
 const Login = () => {
   const { login, isLoggedIn } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isLoggedIn) {
-      navigate('/');
+      navigate("/");
     }
-  }, [isLoggedIn, navigate])
+  }, [isLoggedIn, navigate]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const response = await login(email, password);
-      
-      setEmail('');
-      setPassword('');
+
+      setEmail("");
+      setPassword("");
     } catch (error) {
       console.error(error);
-      setError('Login failed; user does not exist');
+      setError("Login failed; user does not exist");
     }
   };
 
   return (
-    <div className='login-container'>
-      <h1 className='login-message'>Hey, Traveler! Log in to get started on your next adventure!</h1>
+    <div className="login-container">
+      <h1 className="login-message">Login to get started!</h1>
       {error && <p className="error">{error}</p>}
-      <form onSubmit={onSubmit}>
+      <form className="login-form" onSubmit={onSubmit}>
         <p>
-          <input className ='login-input' type='text' name='username'
-            value={email} placeholder='Enter email here'
-            onChange={event => setEmail(event.target.value)} />
+          <label className="login-label">
+            Email:
+            <input
+              className="login-input"
+              type="text"
+              name="username"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-          <input className ='login-input' type='password' name='password'
-            value={password} placeholder='Enter password here'
-            onChange={event => setPassword(event.target.value)} />
+        <label className="login-label">
+          Password:
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          </label>
         </p>
         <p>
-          <button className='login-submit' type='submit' name='submit'>Login</button>
+          <button className="login-submit" type="submit" name="submit">
+            Login
+          </button>
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,112 +1,139 @@
-import { useState, useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../styles/Register.scss";
 
 const RegisterNewUser = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [profilePic, setProfilePic] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [password, setPassword] = useState("");
   const [registrationSuccess, setRegistrationSuccess] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const { login } = useContext(AuthContext);
 
-  const defaultProfilePic = 'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg'
+  const defaultProfilePic =
+    "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg";
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
     if (!firstName || !lastName || !email || !password) {
-      setError('Required field missing');
+      setError("Required field missing");
       return;
     }
 
     try {
       const profilePicUrl = profilePic || defaultProfilePic;
-      const response = await axios.post('http://localhost:8080/register', {
+      const response = await axios.post("http://localhost:8080/register", {
         firstName,
         lastName,
         email,
         profilePic: profilePicUrl,
-        password
+        password,
       });
 
       setRegistrationSuccess(true);
-      
+
       const loginResponse = await login(email, password);
 
       if (loginResponse.status === 200) {
-        navigate('/');
+        navigate("/");
       }
 
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setProfilePic('');
-    setPassword('');
-    setError('');
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setProfilePic("");
+      setPassword("");
+      setError("");
     } catch (error) {
-      console.error('Registration failed', error);
-      setError('Registration failed. Please try again.');
+      console.error("Registration failed", error);
+      setError("Registration failed. Please try again.");
     }
   };
 
   return (
-    <div className='register-container'>
-      <h1 className='register-message'>Hey, Fellow Traveler! Sign up to get started on your next adventure!</h1>
+    <div className="register-container">
+      <h1 className="register-message">Sign up to get started!</h1>
       {error && <p className="error">{error}</p>}
-      {registrationSuccess && <p className="success">You are successfully registered! Please login.</p>}
+      {registrationSuccess && (
+        <p className="success">
+          You are successfully registered! Please login.
+        </p>
+      )}
       <form onSubmit={onSubmit}>
         <p>
-          <input className='register-input'
-          type='text' 
-          name='firstName'
-            value={firstName} 
-            placeholder='First Name'
-            onChange={event => setFirstName(event.target.value)} />
+          <label className="register-label">
+            First Name (Required):
+            <input
+              className="register-input"
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-        <input className='register-input'
-          type='text' 
-          name='lastName'
-            value={lastName} 
-            placeholder='Last Name'
-            onChange={event => setLastName(event.target.value)} />
+          <label className="register-label">
+            Last Name (Required):
+            <input
+              className="register-input"
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-        <input className='register-input'
-          type='email' 
-          name='email'
-            value={email} 
-            placeholder='Enter email here'
-            onChange={event => setEmail(event.target.value)} />
+          <label className="register-label">
+            Email (Required):
+            <input
+              className="register-input"
+              type="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-        <input className='register-input'
-          type='text' 
-          name='profile-pic'
-            value={profilePic} 
-            placeholder='Add a profile picture here'
-            onChange={event => setProfilePic(event.target.value)} />
+          <label className="register-label">
+            Profile Picture (Optional):
+            <input
+              className="register-input"
+              type="text"
+              name="profile-pic"
+              value={profilePic}
+              onChange={(event) => setProfilePic(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-        <input className='register-input'
-          type='password' 
-          name='password'
-            value={password} 
-            placeholder='Enter password here'
-            onChange={event => setPassword(event.target.value)} />
+          <label className="register-label">
+            Password (Required):
+            <input
+              className="register-input"
+              type="password"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
         </p>
         <p>
-          <button className='register-submit' type='submit' name='submit'>Sign Up</button>
+          <button className="register-submit" type="submit" name="submit">
+            Sign Up
+          </button>
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default RegisterNewUser;
